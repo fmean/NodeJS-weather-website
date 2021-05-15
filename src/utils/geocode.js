48023917bde134ab30653abca64e3298d1f189ec
Amin <amin.fazilogly@gmail.com>
2021-05-15 13:12:25 +0200

    request change

--- src/utils/geocode.js
+++ src/utils/geocode.js
@@ -1,4 +1,4 @@
-import request from 'request'
+const request = require('request')
 
 const geocode = (address, callback) => {
     const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + address + '.json?access_token=pk.eyJ1IjoiZm1lYW4iLCJhIjoiY2tvNTJrMmp5MTdzYjJ2czV6YWM1Mml4ZiJ9.ydY9sy-PYif43Om_j4nLug&limit=1'
